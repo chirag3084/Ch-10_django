45ade5843919fd1a646cb119246632413a5187fa
chirag3084 <surtivatsal23@gmail.com>
2026-03-23 08:09:43 +0530

    authenticate

---
+++ authProject/authApp/views.py
@@ -0,0 +1,67 @@
+# To handle views and redirects
+from django.shortcuts import render, redirect
+# To Import auth functions form Django
+from django.contrib.auth import authenticate, login, logout
+# The login_required decorator to protect views
+from django.contrib.auth.decorators import login_required
+# For class-based views[CBV]
+from django.contrib.auth.mixins import LoginRequiredMixin
+# For class-based views[CBV]
+from django.views import View
+#  Import the User class (model)
+from django.contrib.auth.models import User
+# Import the RegisterForm from forms.py
+from .forms import RegisterForm, LoginForm
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = User.objects.create_user(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form':form})
+
+
+def login_view(request):
+    error_message = None 
+    next_url = request.GET.get('next', '')
+    if request.method == "POST": 
+        form = LoginForm(request.POST)
+        next_url = request.POST.get('next') or 'home'
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password") 
+            user = authenticate(request, username=username, password=password)  
+            if user is not None:  
+                login(request, user)  
+                return redirect(next_url) 
+            else:
+                error_message = "Invalid credentials"  
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form':form, 'error': error_message, 'next': next_url})
+
+    
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('login')
+    else:
+        return redirect('home')
+
+# Home View
+# Using the decorator 
+@login_required
+def home_view(request):
+    return render(request, 'auth1_app/home.html')
+
+# Protected View 
+class ProtectedView(LoginRequiredMixin, View):
+    
+    def get(self, request):
+        return render(request, 'registration/protected.html')
